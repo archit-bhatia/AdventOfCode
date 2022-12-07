77a5218244f42fda53f616d2d2ca4190131ad25a
architb23 <abhatia2@atlassian.com>
2022-12-07 17:55:07 +0530

    day6

---
+++ 2022/src/Day6/B.java
@@ -0,0 +1,38 @@
+package Day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class B {
+
+    static int calculateScore(List<String> input) {
+
+        String message = input.get(0);
+
+        for (int i=13; i<message.length(); i++) {
+            Set<Character> letters = new HashSet<>();
+            for (int j=i;j>(i-14);j--) {
+                letters.add(message.charAt(j));
+            }
+
+            if (letters.size() == 14) {
+                return i+1;
+            }
+        }
+
+
+
+        return message.length()-1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String filePath = System.getProperty("user.dir") + "/2022/src/Day6/input.txt";
+
+        List<String> input = Files.readAllLines(Paths.get(filePath));
+
+        System.out.println(calculateScore(input));
+
+    }
+}
