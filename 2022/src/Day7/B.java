cdf0e22f253b8c03a86dda678d99e59b6b38930c
architb23 <abhatia2@atlassian.com>
2022-12-08 13:44:22 +0530

    day7 part 2

--- 2022/src/Day7/B.java
+++ 2022/src/Day7/B.java
@@ -3,34 +3,100 @@ package Day7;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class B {
 
+
+    static class Item {
+        String name;
+        Item parent;
+        List<Item> children;
+        Integer size;
+
+        public Item(String name, Item parent, List<Item> children, Integer size) {
+            this.name = name;
+            this.parent = parent;
+            this.children = children;
+            this.size = size;
+        }
+    }
+
+    static int fillSizeMap(Item item) {
+        // base case
+        if (item.children.isEmpty()) {
+            return item.size;
+        }
+
+        // recursive case
+        int sum = 0;
+        for (var children : item.children) {
+            sum += fillSizeMap(children);
+        }
+        item.size = sum;
+
+        return sum;
+    }
+
     static int calculateScore(List<String> input) {
 
-        String message = input.get(0);
+        List<Item> items = new ArrayList<>();
 
-        for (int i=13; i<message.length(); i++) {
-            Set<Character> letters = new HashSet<>();
-            for (int j=i;j>(i-14);j--) {
-                letters.add(message.charAt(j));
-            }
+        items.add(new Item("/", null, new ArrayList<>(), 0));
 
-            if (letters.size() == 14) {
-                return i+1;
+        var currDir = items.get(0);
+        var arg = "";
+
+        for (var line : input) {
+            if (line.isBlank()) break;
+
+            if (line.charAt(0) == '$' && line.charAt(2) == 'c') {
+                arg = line.substring(5);
+                if (arg.equals("..")) {
+                    currDir = currDir.parent;
+                } else {
+                    for (var item : currDir.children) {
+                        if (item.name.equals(arg)) {
+                            currDir = item;
+                            break;
+                        }
+                    }
+                }
+            } else if (line.charAt(0) != '$') {
+                var tokens = line.split(" ");
+                var newItem = new Item(tokens[1], currDir, new ArrayList<>(), 0);
+
+                if (!tokens[0].equals("dir")) {
+                    newItem.size = Integer.parseInt(tokens[0]);
+                }
+                currDir.children.add(newItem);
+                items.add(newItem);
             }
         }
 
+        fillSizeMap(items.get(0));
+        int ans = Integer.MAX_VALUE;
+
+        int totalSpace = 70000000;
+        int spaceRequiredForUpdate = 30000000;
+        int currentFreeSpace = totalSpace - items.get(0).size;
+        int spaceNeeded = spaceRequiredForUpdate - currentFreeSpace;
+
+        for (var item : items) {
+            // Not a dir
+            if (item.children.isEmpty()) continue;
 
 
-        return message.length()-1;
+            if (item.size >= spaceNeeded) {
+                ans = Integer.min(item.size, ans);
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) throws IOException {
-        String filePath = System.getProperty("user.dir") + "/2022/src/Day6/input.txt";
+        String filePath = System.getProperty("user.dir") + "/2022/src/Day7/input.txt";
 
         List<String> input = Files.readAllLines(Paths.get(filePath));
 
